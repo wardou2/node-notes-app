1fac3129b1017c37f4b34b92e2047fca982f8759
wardou2 <ward92@gmail.com>
2019-08-29 17:16:29 -0700

    require chalk working

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const validator = require('validator')
+const chalk = require('chalk')
+
 const getNotes = require('./notes.js')
 
-console.log(getNotes());
+console.log(chalk.green.inverse.bold(getNotes()))
+
 console.log(validator.isURL('example.com'))
